f8f08d9d68ffb681f259730772e22b3e3daa4a8f
5600 MHz <ram3400mhz@gmail.com>
2025-12-14 23:59:58 -0300

    Video 01
    Básicos: print + comentarios

---
+++ 01 - Basicos.py
@@ -0,0 +1,20 @@
+#########################
+#                       #
+# Aprende Python en 60s #
+# 5600 MHz              #
+#                       #
+#########################
+
+# Comentario
+
+""" Usa comillas para un
+comentario multilinea """
+
+# Imprimir en pantalla
+print("Hello, World")
+
+# Explicacion
+# print() es una funcion
+# "Hello, World" es un string
+# En este caso, el string se usa
+# de parametro para print()
