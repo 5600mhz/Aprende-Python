a223f2ad49bd8a32fac5ac01e05296c115a9f20c
5600 MHz <ram3400mhz@gmail.com>
2025-12-15 20:03:58 -0300

    Short 02 - Variables
    Explicación introductoria a variables y tipos de datos

---
+++ 02 - Variables.py
@@ -0,0 +1,34 @@
+#########################
+#                       #
+# Aprende Python en 60s #
+# 5600 MHz              #
+#                       #
+#########################
+
+variable = "valor"
+
+print(variable)
+
+print("El valor es", variable)
+
+# Reglas para nombrar variables
+# - Debe comenzar con letra o _ NO numeros
+# - Despues, se pueden letras, nums y _
+# - Nunca guion medio (-) u otros simbolos
+# - Nunca uses espacios
+
+# Diferentes tipos de datos
+
+# String (str)
+mi_texto = "Hello, World"
+
+# Int/Entero (int)
+numero = 25
+
+# Float/Flotante (float)
+numero_decimal = 3.1415
+
+# Bool/Booleano (bool)
+# Notese las mayusculas
+si_no = True
+si_no = False
