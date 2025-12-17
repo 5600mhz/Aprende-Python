d48445acd4f5fae61637b39883e642a56ae21f91
5600 MHz <ram3400mhz@gmail.com>
2025-12-17 14:43:25 -0300

    Video 03 - Tipos de datos
    Conversión y type()

---
+++ 03 - Tipos de datos.py
@@ -0,0 +1,32 @@
+#########################
+#                       #
+# Aprende Python en 60s #
+# 5600 MHz              #
+#                       #
+#########################
+
+# Funcion type()
+numero = 5600
+texto = "Hello, World"
+pi = 3.14
+
+print(type(numero)) # int
+print(type(texto)) # str
+print(type(pi)) # float
+
+
+# Conversion de tipos
+
+# str() convierte a string
+numero_string = str(1234)
+print(type(numero_string))
+
+# int() convierte a entero
+numero_int = int("25")
+print(type(numero_int))
+
+numero_float = int(3.14)
+print(type(numero_float))
+
+# Daria error con una letra
+# num = int("a")
