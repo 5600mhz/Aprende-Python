d432e41d5043edc2f8d9476abaaf8494f0e50083
5600 MHz <ram3400mhz@gmail.com>
2025-12-20 16:54:26 -0300

    Video 04 - Operaciones Matematicas
    Explicación de 6 operadores mat.

---
+++ 04 - Operaciones Matematicas.py
@@ -0,0 +1,23 @@
+#########################
+#                       #
+# Aprende Python en 60s #
+# 5600 MHz              #
+#                       #
+#########################
+
+# Matematica
+
+# Suma
+suma = 1 + 5 # 6
+# Resta
+resta = suma - 5 # 1
+# Division
+division = 27 / 3 # 9.0 (float)
+# Multiplicacion
+multiplicacion = 9 * 3 # 27
+# Division entera
+div_ent = 27 // 3 # 9 (int)
+# Potencia
+pow = 2 ** 8 # = 2^8 = 256
+# Modulo (resto de division)
+print(28 % 3) # 1
