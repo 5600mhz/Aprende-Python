831e561241c9037090d86da6fda8cb69ebd8ba78
5600 MHz <ram3400mhz@gmail.com>
2025-12-21 23:02:15 -0300

    Video 05 - Operadores de Comparacion
    Operadores de comparación de datos

---
+++ 05 - Operadores de Comparacion.py
@@ -0,0 +1,39 @@
+#########################
+#                       #
+# Aprende Python en 60s #
+# 5600 MHz              #
+#                       #
+#########################
+
+# Operadores de Comparacion
+
+num1 = 5
+num2 = 6
+
+# Igual que (==)
+# False, son diferentes
+print(num1 == num2)
+
+# Distinto que (!=)
+# True, son diferentes
+print(num1 != num2)
+
+# Mayor que (>)
+# False, num1 NO es mayor
+print(num1 > num2)
+
+# Menor que (<)
+# True, num1 es menor
+print(num1 < num2)
+
+# Mayor o igual que (>=)
+# False, num1 NO es mayor o igual
+print(num1 >= num2)
+
+# Menor o igual que (<=)
+# True, num1 es menor o igual
+print(num1 <= num2)
+
+
+# Este codigo y anteriores
+# en el repo de Github
