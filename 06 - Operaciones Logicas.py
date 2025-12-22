c17a17f53f7bb156b6a5c0ebda26fd353858a4a9
5600 MHz <ram3400mhz@gmail.com>
2025-12-22 16:40:46 -0300

    Video 06 - Operaciones Logicas
    Explicación de ops. AND, OR, NOT.
    Uso en print y paréntesis.

---
+++ 06 - Operaciones Logicas.py
@@ -0,0 +1,29 @@
+#########################
+#                       #
+# Aprende Python en 60s #
+# 5600 MHz              #
+#                       #
+#########################
+
+# Operadores Logicos
+
+condicion1 = True
+condicion2 = False
+
+# OR devuelve True si una de
+# las condiciones evaluadas es
+# True
+print(condicion1 or condicion2)
+
+# AND devuelve True si AMBAS
+# condiciones son True
+print(condicion1 and condicion2)
+
+# NOT devuelve True si la
+# condicion evaluada es False
+print(not condicion2)
+
+
+# Se pueden combinar con
+# parentesis
+print(not (True and False))
