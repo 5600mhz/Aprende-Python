961001bae7a11b186d79a5eb9ba3a2299def1210
5600 MHz <ram3400mhz@gmail.com>
2025-12-24 00:46:48 -0300

    Video 07 - Strings
    Funcion len() y métodos 
    Explicación f-strings

---
+++ 07 - Strings.py
@@ -0,0 +1,39 @@
+#########################
+#                       #
+# Aprende Python en 60s #
+# 5600 MHz              #
+#                       #
+#########################
+
+# Strings
+
+
+# Funcion len()
+# Devulve la longitud del
+# string dado
+print(len("Hola")) # 4
+
+# Metodos
+
+# .upper()
+# Convierte el string a
+# mayusculas
+print("hola".upper()) # HOLA
+
+# .lower()
+# Convierte el string a
+# minisuculas
+print("HOLA".lower()) # hola
+
+# .capitalize()
+# Convierte la primera letra
+# en mayuscula
+print("hello world".capitalize())
+# = Hello world
+
+
+# Formato
+# F-Strings
+var = 123
+
+print(f"El valor es: {var}")
