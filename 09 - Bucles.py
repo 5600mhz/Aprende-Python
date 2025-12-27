e1ff4be26bdeeb223d89cb8e39fcc883de50a317
5600 MHz <ram3400mhz@gmail.com>
2025-12-27 19:04:20 -0300

    Video 09 - Bucles
    Explicacion de bucles while y for

---
+++ 09 - Bucles.py
@@ -0,0 +1,31 @@
+#########################
+#                       #
+# Aprende Python en 60s #
+# 5600 MHz              #
+#                       #
+#########################
+
+# Bucles
+
+
+# Bucle While
+
+numero = 1
+
+# Mientras numero <= 10
+while numero <= 10:
+    print(f"Numero: {numero}")
+    # Aumentar en 1
+    numero = numero + 1
+
+
+# Bucle For
+texto = "Hello, World!"
+
+for i in texto:
+    print(i)
+
+
+
+# Otra forma de aumentar en 1
+numero += 1
